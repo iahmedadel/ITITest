1d76b90076b4794f064d7649703ccedf6f7dbe3a
ahmed <ahmedadel.2003816@gmail.com>
2024-08-06 07:45:21 +0300

    iti

--- ITITest/ViewController.swift
+++ ITITest/ViewController.swift
@@ -1,19 +1,27 @@
-//
-//  ViewController.swift
-//  ITITest
-//
-//  Created by MacBook Pro on 05/08/2024.
-//
+
 
 import UIKit
 
+
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
-}
+    @IBAction func btn1(_ sender: Any) {
+        if txtfield.text == "2"{
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            let vc = sb.instantiateViewController(withIdentifier: "Screen2")
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        
+    }
+    
+    }
 
